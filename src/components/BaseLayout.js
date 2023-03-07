a51d8838a5eaa7c00e1af90a6d48f2fe4bf20648
Lance Soundy <soundy09@gmail.com>
2023-03-07 10:16:01 -0500

    modified:   src/components/BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import Style from './BaseLayout.module.scss'
+// import Style from './BaseLayout.module.scss'
 import Navbar from "./Navbar";
 import Home from "./home/Home";
 import About from "./about/About";
@@ -8,24 +8,24 @@ import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
 
 export default function BaseLayout() {
-   let [darkMode, setDarkMode] = useState(false);
+   // let [darkMode, setDarkMode] = useState(false);
 
-   function handleToggleDarkMode() {
-      let oppositeOfCurrentDarkMode = !darkMode
-      console.log(oppositeOfCurrentDarkMode)
-      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
-      setDarkMode(oppositeOfCurrentDarkMode)
-   }
+   // function handleToggleDarkMode() {
+   //    let oppositeOfCurrentDarkMode = !darkMode
+   //    console.log(oppositeOfCurrentDarkMode)
+   //    localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
+   //    setDarkMode(oppositeOfCurrentDarkMode)
+   // }
 
-   useEffect(() => {
-      let detectedDarkMode = eval(localStorage.getItem('darkMode'));
+   // useEffect(() => {
+   //    let detectedDarkMode = eval(localStorage.getItem('darkMode'));
 
-      if (detectedDarkMode) {
-         setDarkMode(detectedDarkMode)
-      } else {
-         localStorage.setItem('darkMode', 'false')
-      }
-   }, [])
+   //    if (detectedDarkMode) {
+   //       setDarkMode(detectedDarkMode)
+   //    } else {
+   //       localStorage.setItem('darkMode', 'false')
+   //    }
+   // }, [])
 
    return (
       // <Box className={darkMode ? Style.dark : Style.light}>
@@ -33,7 +33,8 @@ export default function BaseLayout() {
          <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
                justifyContent={'space-between'}>
             <Grid item>
-               <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode}/>
+               <Navbar/>
+               {/* // darkMode={darkMode} handleClick={handleToggleDarkMode}/> */}
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
